ce72194be161237ac39bdefcf492b877edeead81
Adrian <awkirchner@gmail.com>
2020-03-05 23:16:01 -0600

    Problems 10-11

---
+++ Haskell/010.hs
@@ -0,0 +1,14 @@
+-- The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+-- Find the sum of all the primes below two million.
+
+isPrime :: Int -> Bool -- change this to use primes.
+isPrime 0 = False
+isPrime 1 = False
+isPrime 2 = True
+isPrime x = not $ or $ map (==0) [mod x p | p <- takeWhile ((<= sqrt (fromIntegral x)) . fromIntegral) primes]
+
+primes = filter isPrime [1..]
+
+p010 = do
+    print $ sum $ takeWhile (< 2000000) primes
