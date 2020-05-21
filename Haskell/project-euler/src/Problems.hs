000b8cae15c985173e02209c0c4a2e656b85a8cb
Adrian <awkirchner@gmail.com>
2020-05-20 23:35:56 -0500

    Problem 25

--- Haskell/project-euler/src/Problems.hs
+++ Haskell/project-euler/src/Problems.hs
@@ -29,7 +29,7 @@ p001 = sum $ mergeNoDups [3, 6..999] [5, 10..999]
 -- By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
 genFibs :: Num a => a -> a -> [a]
-genFibs x y = (:) x $ genFibs y $ x + y
+genFibs x y = (x :) . genFibs y $ x + y
 
 p002 = sum $ filter even $ takeWhile (<=4000000) $ genFibs 1 2
 
@@ -815,3 +815,31 @@ intPermutations :: [Int] -> [Int]
 intPermutations = sort . map read . permutations . concat . map show
 
 p024 = intPermutations [0..9] !! 999999
+
+
+-- Problem 25
+-- The Fibonacci sequence is defined by the recurrence relation:
+
+-- F(n) = F(n−1) + F(n−2), where F(1) = 1 and F(2) = 1.
+-- Hence the first 12 terms will be:
+
+-- F(1) = 1
+-- F(2) = 1
+-- F(3) = 2
+-- F(4) = 3
+-- F(5) = 5
+-- F(6) = 8
+-- F(7) = 13
+-- F(8) = 21
+-- F(9) = 34
+-- F(10) = 55
+-- F(11) = 89
+-- F(12) = 144
+-- The 12th term, F(12), is the first term to contain three digits.
+
+-- What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+numDigits :: Integer -> Int
+numDigits = length . show
+
+p025 = let Just (i, x) = find ((== 1000) . numDigits . snd) . zip [1..] $ genFibs 1 1 in i
