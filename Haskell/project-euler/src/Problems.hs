8ee9f327aa89e3c203dec8b27444a634d9f30b55
Adrian <awkirchner@gmail.com>
2020-05-20 23:07:42 -0500

    Problem 24

--- Haskell/project-euler/src/Problems.hs
+++ Haskell/project-euler/src/Problems.hs
@@ -801,3 +801,17 @@ isSumOfAbundants :: Int -> Bool
 isSumOfAbundants n = not $ null [ x | x <- takeWhile (< (div n 2 + 1)) abundantNumbers, isAbundant (n - x) ]
 
 p023 = sum $ filter (not . isSumOfAbundants) [1..28123]
+
+
+-- Problem 24
+-- A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+-- If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+-- 012   021   102   120   201   210
+
+-- What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+intPermutations :: [Int] -> [Int]
+intPermutations = sort . map read . permutations . concat . map show
+
+p024 = intPermutations [0..9] !! 999999
