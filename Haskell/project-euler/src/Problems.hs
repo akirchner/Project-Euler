584a3680c4cb032a6f8e646b81380931befa4ac0
Adrian <awkirchner@gmail.com>
2020-05-20 22:51:18 -0500

    Problem 23

--- Haskell/project-euler/src/Problems.hs
+++ Haskell/project-euler/src/Problems.hs
@@ -774,3 +774,30 @@ stringValue = sum . map cValue
 
 p022 :: String -> Int
 p022 fileContents = sum $ zipWith (\p s -> p * stringValue s) [1..] $ sort $ filter ((>0) . length) $ splitOn "," $ filter (/= '\"') fileContents
+
+
+-- Problem 23
+-- A perfect number is a number for which the sum of its proper divisors is exactly equal to the number.
+-- For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+-- A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+-- As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24.
+-- By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers.
+-- However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be
+-- expressed as the sum of two abundant numbers is less than this limit.
+
+-- Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+properDivisors :: Int -> [Int]
+properDivisors = init . getDivisors
+
+isAbundant :: Int -> Bool
+isAbundant n = n < sum (properDivisors n)
+
+abundantNumbers = filter (isAbundant) [1..]
+
+isSumOfAbundants :: Int -> Bool
+isSumOfAbundants n = not $ null [ x | x <- takeWhile (< (div n 2 + 1)) abundantNumbers, isAbundant (n - x) ]
+
+p023 = sum $ filter (not . isSumOfAbundants) [1..28123]
