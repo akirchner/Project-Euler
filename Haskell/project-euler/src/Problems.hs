646ce1a240a6e9277aa281eba8a638b32b7790cc
Adrian <awkirchner@gmail.com>
2020-05-21 20:03:42 -0500

    Problem 26

--- Haskell/project-euler/src/Problems.hs
+++ Haskell/project-euler/src/Problems.hs
@@ -843,3 +843,38 @@ numDigits :: Integer -> Int
 numDigits = length . show
 
 p025 = let Just (i, x) = find ((== 1000) . numDigits . snd) . zip [1..] $ genFibs 1 1 in i
+
+
+-- Problem 26
+-- A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
+
+-- 1/2	= 	0.5
+-- 1/3	= 	0.(3)
+-- 1/4	= 	0.25
+-- 1/5	= 	0.2
+-- 1/6	= 	0.1(6)
+-- 1/7	= 	0.(142857)
+-- 1/8	= 	0.125
+-- 1/9	= 	0.(1)
+-- 1/10	= 	0.1
+-- Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
+
+-- Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
+multiplicativeOrder :: Int -> Int -> Int
+multiplicativeOrder _ 1 = 0
+multiplicativeOrder a n = fromInteger $ order 1 (toInteger a) (toInteger n)
+    where order k a' n' | mod a' n' == 1 = k
+                        | otherwise      = order (k + 1) (a' * toInteger a) n'
+
+makeCoprimeTo :: Int -> Int -> Int
+makeCoprimeTo a b = reduce b (primeFactors a)
+    where reduce 1 _  = 1
+          reduce b [] = b
+          reduce b (f:fs) | mod b f == 0 = reduce (div b f) (f:fs)
+                          | otherwise    = reduce b fs
+
+numReciprocalDigits :: Int -> Int
+numReciprocalDigits = multiplicativeOrder 10 . makeCoprimeTo 10
+
+p026 = fst . maximumBy (\(_, x) (_, y) -> compare x y) . zip [2..999] $ map numReciprocalDigits [2..999]
